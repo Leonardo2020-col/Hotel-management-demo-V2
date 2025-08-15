ee60844331a7387dc108c278557dc4a5a2269698
Leonardo2020-col <leonardopareja80@gmail.com>
2025-08-14 19:17:41 -0500

    page dashboard 5

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,7 +1,7 @@
 // src/components/layout/Layout.jsx
 import React, { useState } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
-import { useAuth } from '../../context/AuthContext'
+import { useAuth } from '../context/AuthContext'
 import { 
   Hotel,
   Home,
