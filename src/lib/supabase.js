674daa7c9bfdda3667bf6c9730f9c45c42455b22
Leonardo2020-col <leonardopareja80@gmail.com>
2025-09-20 14:33:35 -0500

    update branch 10

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
+import { branchService as externalBranchService } from './branchService'
+
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
